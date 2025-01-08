6ae1cc990e88e5fe3d80a98644eb1b0b65e5f743
BarbarianBunny <carury@gmail.com>
2025-01-07 20:20:41 -0800

    add tests for textnode

--- src/textnode.py
+++ src/textnode.py
@@ -1,19 +1,17 @@
 from enum import Enum
 
-class Bender(Enum):
-    AIR_BENDER = "air"
-    WATER_BENDER = "water"
-    EARTH_BENDER = "earth"
-    FIRE_BENDER = "fire"
+class TextType(Enum):
+    BOLD = "bold"
+    ITALIC = "italic"
 
 class TextNode():
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
     def __eq__(self, value):
-        return self == value
+        return str(self) == str(value)
     
     def __repr__(self):
-        return f"TextNode(\"{self.text}\", {self.text_type}, \"{self.url}\")"
+        return f"TextNode(\"{self.text}\", {self.text_type.value}, \"{self.url}\")"
