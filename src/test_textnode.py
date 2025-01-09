f8e2a6d2279fb4cd194226a47ef24a25d4244f28
BarbarianBunny <carury@gmail.com>
2025-01-09 04:17:07 -0800

    add TextNode to HTMLNode method and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,7 +1,7 @@
 import unittest
 
 from textnode import TextNode, TextType
-
+from htmlnode import LeafNode
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -22,6 +22,38 @@ class TestTextNode(unittest.TestCase):
     def test_empty_url_none(self):
         node = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node.url, None)
+
+class TestToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("Test", TextType.TEXT)
+        node2 = LeafNode(None, "Test")
+        self.assertEqual(node.to_html_node(), node2)
+
+    def test_bold(self):
+        node = TextNode("Test", TextType.BOLD)
+        node2 = LeafNode("b", "Test")
+        self.assertEqual(node.to_html_node(), node2)
+
+    def test_code(self):
+        node = TextNode("Test", TextType.CODE)
+        node2 = LeafNode("code", "Test")
+        self.assertEqual(node.to_html_node(), node2)
+
+    def test_italic(self):
+        node = TextNode("Test", TextType.ITALIC)
+        node2 = LeafNode("i", "Test")
+        self.assertEqual(node.to_html_node(), node2)
+
+    def test_image(self):
+        node = TextNode("Test", TextType.IMAGE, "www.image.com")
+        node2 = LeafNode("img", "", {"src": "www.image.com", "alt": "Test"})
+        self.assertEqual(node.to_html_node(), node2)
+
+    def test_link(self):
+        node = TextNode("Test", TextType.LINK, "www.link.com")
+        node2 = LeafNode("a", "Test", {"href": "www.link.com"})
+        self.assertEqual(node.to_html_node(), node2)
+
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,8 +1,16 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
-    BOLD = "bold"
-    ITALIC = "italic"
+    def __str__(self):
+        return str(self.value)
+    
+    TEXT = ""
+    LINK = "a"
+    BOLD = "b"
+    CODE = "code"
+    ITALIC = "i"
+    IMAGE = "img"
 
 class TextNode():
     def __init__(self, text, text_type, url=None):
@@ -15,3 +23,18 @@ class TextNode():
     
     def __repr__(self):
         return f"TextNode(\"{self.text}\", {self.text_type.value}, \"{self.url}\")"
+
+    def to_html_node(text_node):
+        match text_node.text_type:
+            case TextType.TEXT:
+                return LeafNode(None, text_node.text)
+            case TextType.LINK:
+                if text_node.url is None:
+                    raise ValueError("Invalid LINK: no url")
+                return LeafNode(str(text_node.text_type), text_node.text, {"href": text_node.url})
+            case TextType.IMAGE:
+                if text_node.url is None:
+                    raise ValueError("Invalid IMAGE: no src url")
+                return LeafNode(str(text_node.text_type), "", {"src": text_node.url, "alt": text_node.text})
+
+        return LeafNode(str(text_node.text_type), text_node.text)
